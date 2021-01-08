13993f0deda9e30fb921fcf1278c5cfe856db314
李廷琛 <2496272528@qq.com>
2021-01-09 00:25:56 +0800

    配置sass全局变量以及按需导入antd

---
+++ config-overrides.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: Your name
+ * @Date:   2021-01-08 00:17:16
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2021-01-09 00:23:10
+ */
+const { override, adjustStyleLoaders,fixBabelImports } = require("customize-cra");
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd', libraryDirectory: 'es', style: 'css'
+    }),
+    // ...其他配置...
+    adjustStyleLoaders(rule => {
+        if (rule.test.toString().includes("scss")) {
+            rule.use.push({
+                loader: require.resolve("sass-resources-loader"),
+                options: {
+                    resources: "./src/styles/main.scss" //这里是你自己放公共scss变量的路径
+                }
+            });
+        }
+    })
+    // ...其他配置...
+);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import logo from './logo.svg';
-import './App.scss';
+/*
+ * @Author: Your name
+ * @Date:   2021-01-07 00:55:07
+ * @Last Modified by:   Your name
+ * @Last Modified time: 2021-01-08 00:40:09
+ */
 
+import {  Route, HashRouter, Switch } from 'react-router-dom'
+import Login from './views/Login'
+import Info from './views/Info'
+import Home from './views/Home'
+import React from 'react'
 function App() {
-  return (
-    <div className="App">
-      <h1>4444</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <HashRouter>
+                <Switch>
+                    <Route component={Home} exact path="/" />
+                    <Route component={Login} path="/login" />
+                    <Route component={Info} path="/info" />
+                </Switch>
+            </HashRouter>
+            {/*<h1>4444</h1>*/}
+            {/*<header className="App-header">*/}
+            {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+            {/*  <p>*/}
+            {/*    Edit <code>src/App.js</code> and save to reload.*/}
+            {/*  </p>*/}
+            {/*  <a*/}
+            {/*    className="App-link"*/}
+            {/*    href="https://reactjs.org"*/}
+            {/*    target="_blank"*/}
+            {/*    rel="noopener noreferrer"*/}
+            {/*  >*/}
+            {/*    Learn React*/}
+            {/*  </a>*/}
+            {/*</header>*/}
+        </div>
+    );
 }
 
 export default App;
