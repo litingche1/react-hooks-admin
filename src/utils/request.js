e2dac692d7e3389ece902153ddbc62ab6bf33f0b
李廷琛 <2496272528@qq.com>
2021-01-20 00:17:51 +0800

    响应拦全局拦截

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { getToken, getUsername } from './cookies'
+import { message } from 'antd'
 //第一步创建实例
 const serves = axios.create({
     baseURL: process.env.REACT_APP_API,
@@ -22,9 +23,15 @@ serves.interceptors.request.use(function (config) {
 //第三步响应拦截
 // 添加响应拦截器
 serves.interceptors.response.use(function (response) {
+    let data = response.data
+    if (data.resCode === 0) {
+        return response
+    } else {
+        message.error(data.message)
+        return Promise.reject(response);
+    }
     // 对响应数据做点什么
-    return response;
-}, function (error) {
+}, function (error) {//http状态不为200的情况
     // 对响应错误做点什么
     return Promise.reject(error);
 });
